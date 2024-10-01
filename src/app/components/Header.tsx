0c59e099004a302ab863d3a14d140f59b4a433e5
joshfarhi <joshuafarhi@protonmail.com>
2024-10-01 15:23:33 -0400

    makin progress

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,43 +10,6 @@ import { routes, display } from '@/app/resources'
 import { person, home, about, blog, work, gallery } from '@/app/resources'
 import React from "react";
 
-type TimeDisplayProps = {
-    timeZone: string;
-    locale?: string;  // Optionally allow locale, defaulting to 'en-GB'
-};
-
-const TimeDisplay: React.FC<TimeDisplayProps> = ({ timeZone, locale = 'en-GB' }) => {
-    const [currentTime, setCurrentTime] = useState('');
-
-    useEffect(() => {
-        const updateTime = () => {
-            const now = new Date();
-            const options: Intl.DateTimeFormatOptions = {
-                timeZone,
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-                hour12: false,
-            };
-            const timeString = new Intl.DateTimeFormat(locale, options).format(now);
-            setCurrentTime(timeString);
-        };
-
-        updateTime();
-        const intervalId = setInterval(updateTime, 1000);
-
-        return () => clearInterval(intervalId);
-    }, [timeZone, locale]);
-
-    return (
-        <>
-            {currentTime}
-        </>
-    );
-};
-
-export default TimeDisplay;
-
 export const Header = () => {
     const pathname = usePathname() ?? '';
 
@@ -57,15 +20,7 @@ export const Header = () => {
             zIndex={9}
             fillWidth padding="8"
             justifyContent="center">
-            <Flex
-                hide="s"
-                paddingLeft="12" fillWidth
-                alignItems="center"
-                textVariant="body-default-s">
-                { display.location && (
-                    <>{person.location}</>
-                )}
-            </Flex>
+            
             <Flex
                 background="surface" border="neutral-medium" borderStyle="solid-1" radius="m-4" shadow="l"
                 padding="4"
@@ -114,15 +69,6 @@ export const Header = () => {
                         </ToggleButton>
                     )}
                 </Flex>
-            </Flex>
-            <Flex
-                hide="s"
-                paddingRight="12" fillWidth
-                justifyContent="flex-end" alignItems="center"
-                textVariant="body-default-s">
-                { display.time && (
-                    <TimeDisplay timeZone={person.location}/>
-                )}
             </Flex>
         </Flex>
     )
