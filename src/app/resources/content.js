1438e8b78622d9a477d828d8551dad45163ff445
joshfarhi <joshuafarhi@protonmail.com>
2024-10-02 00:30:26 -0400

    some more updates

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -145,7 +145,7 @@ const about = {
                 description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
                 images: [
                     {
-                        src: '/images/projects/project-01/cover-04.jpg',
+                        src: '/images/projects/project-01/cover-04.png',
                         alt: 'Project image',
                         width: 16,
                         height: 9
