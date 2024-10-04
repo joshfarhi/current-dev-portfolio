6b18e357c3ad8789c417f6198a93122d5feb9c8d
joshfarhi <joshuafarhi@protonmail.com>
2024-10-03 22:40:41 -0400

    updates

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -23,7 +23,7 @@ const effects = {
 const style = {
     theme:       'dark',         // dark | light
     neutral:     'gray',         // sand | gray | slate
-    brand:       'aqua',         // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    brand:       'magenta',         // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     accent:      'cyan',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
     solid:       'contrast',     // color | contrast
     solidStyle:  'plastic',         // flat | plastic
