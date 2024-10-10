3d35f74cf395f5c9419ef3308a56d7e23701b1f8
joshfarhi <joshuafarhi@protonmail.com>
2024-10-10 16:17:14 -0400

    updated avatar

--- src/app/resources/content-blog.js
+++ src/app/resources/content-blog.js
@@ -8,7 +8,7 @@ const person = {
         return `${this.firstName} ${this.lastName}`;
     },
     role:      'Software Engineer',
-    avatar:    '/images/avatar.jpg',
+    avatar:    '/images/avatar.jpeg',
     location:  'America/NewYork',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
 }
