71063b2dd4a7adf4038d61d8e9e77bfeb328c664
Joshua <joshuafarhi@protonmail.com>
2024-10-21 13:23:28 -0700

    random edits

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -70,9 +70,9 @@ const about = {
         display: true,
         title: 'Introduction',
         description: <>
-        Joshua Farhi is a Next.js front-end developer and web engineer driven by a passion for simplifying complex challenges into clean, intuitive design solutions. 
+        Joshua Farhi is a front-end developer and web engineer driven by a passion for simplifying complex challenges into clean, intuitive design solutions. 
         He excels in creating seamless user experiences through the interfaces he both builds and designs using Next.js and modern JavaScript frameworks.
-         His work also includes comprehensive SEO analysis, and he has extensive experience in Digital Design with the Adobe Creative Suite. Joshua is dedicated to blending performance optimization and responsive design to ensure fast, interactive, and visually appealing web applications.
+        His work also includes comprehensive SEO analysis, and he has extensive experience in Digital Design with the Adobe Creative Suite. Joshua is dedicated to blending performance optimization and responsive design to ensure fast, interactive, and visually appealing web applications.
         </>
     },
     work: {
