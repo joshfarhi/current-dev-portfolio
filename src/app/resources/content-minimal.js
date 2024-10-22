96ceaac3ec6ce101ac02bf3ffa92e20376d1bc95
joshfarhi <joshuafarhi@protonmail.com>
2024-10-22 13:56:05 -0700

    edited poster

--- src/app/resources/content-minimal.js
+++ src/app/resources/content-minimal.js
@@ -89,7 +89,7 @@ const about = {
                 ],
                 images: [ // optional: leave the array empty if you don't want to display images
                     {
-                        src: '/images/projects/project-01/cover-01.jpg',
+                        src: '/images/projects/project-01/Invemtory-Tracker-Poster.png',
                         alt: 'Once UI Project',
                         width: 16,
                         height: 9
